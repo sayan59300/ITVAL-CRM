41d5fd221dfd426e6b4ce10b6593d7fe0243b8be
Buffart Nicolas <nicolas.buffart@itval.fr>
2022-04-05 09:51:51 +0200

    init

---
+++ src/main/java/itval/cie/ItvalCrmApplication.java
@@ -0,0 +1,13 @@
+package itval.cie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItvalCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItvalCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/itval/cie/ItvalCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package itval.cie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItvalCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
